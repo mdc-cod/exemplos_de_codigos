ecb3d2efb8b328366bcab52c0e5a173d733cc900
mdc-cod <mdcoconta@gmail.com>
2026-05-03 18:15:32 -0300

    programação soma.c alterado
    Updated printf statement to include label 'resultado:'

--- C/soma.c
+++ C/soma.c
@@ -2,9 +2,10 @@
 
 int main(){
 
-int n1=10;
-int n2=5;
-int soma = n1+n2;
-printf("%d", soma);
+  int n1=10;
+  int n2=5;
+  int soma = n1+n2;
+     printf("resultado: %d", soma);
+  return 0;
 
 }
