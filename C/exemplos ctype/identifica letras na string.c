6c140699ff90cfde252afe6ca4a16f88bacb920c
mdc-cod <mdcoconta@gmail.com>
2026-05-14 19:21:54 -0300

    Update string to include additional characters

--- C/exemplos ctype/identifica letras na string.c
+++ C/exemplos ctype/identifica letras na string.c
@@ -5,12 +5,13 @@
 
 int main(){
 
-    char texto[] = "abcd1234";
+    char texto[] = "abcd1234efgh";
 
     for(int i=0; texto[i]!='\0';i++){
 
+    // identifica as letras no texto 
     if(isalpha(texto[i])==1)
-       printf("%c, ", texto[i]); // exibe apenas as letras no texto
+       printf("%c, ", texto[i]); // exibe as letras encontradas 
 
     }
 }
