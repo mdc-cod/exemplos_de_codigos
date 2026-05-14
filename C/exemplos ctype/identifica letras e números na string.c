8c62080b771b3e6103a17a8aad30a0a2cc50fc7a
mdc-cod <mdcoconta@gmail.com>
2026-05-14 19:27:40 -0300

    Adiciona programa para identificar letras e números
    Implementa um programa que identifica e exibe letras e números em uma string.

---
+++ C/exemplos ctype/identifica letras e números na string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+
+int main(){
+
+    char texto[] = "abcd1234!@#+/*";
+
+    for(int i=0; texto[i]!='\0';i++){
+
+    // identifica letras e números apenas
+    if(isalnum(texto[i])==1)
+       printf("%c,", texto[i]); // exibe as letras e números encontrados 
+
+    }
+
+
+}
