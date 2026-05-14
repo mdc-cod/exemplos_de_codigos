17a12163a7671654815228caddbfcbbb8fba8023
mdc-cod <mdcoconta@gmail.com>
2026-05-14 19:13:58 -0300

    Update string to include additional characters and format

---
+++ C/identifica dígitos na string.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <ctype.h>
+
+
+int main(){
+
+    char texto[] = "abcd1234efgh";
+
+    for(int i=0; texto[i]!='\0';i++){
+
+    if(isdigit(texto[i])==1)
+       printf("%c, ", texto[i]);
+
+    }
+
+
+}
