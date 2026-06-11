cf61d913eb6f81dde228f5db8eff2c1e3f95ca7c
mdc-cod <mdcoconta@gmail.com>
2026-06-11 14:23:21 -0300

    arquivo islower-verifica letra minúscula.c criado
    exemplo de uso da função islower da biblioteca ctype.h.

---
+++ C/exemplos ctype/islower-verifica letras minúsculas.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+
+    char c='c';
+    if(islower(c)==1){
+
+     printf("letra minúscula.\n");
+      
+    }
+   char a='a';
+   int tipo_letra = islower(a);
+   if(tipo_letra == 1){
+
+     printf("letra minuscula\n");
+     
+   }
+
+   char str[]="Exemplo de Uso De islower";
+   
+   for(int i=0; str[i]!='\0';i++){
+      if(islower(str[i])==1){
+        // exibe apenas letras minúsculas em str[]
+        printf("letras minusculas: %c", str[i]);
+      }
+   }
+
+  return 0;
+}
