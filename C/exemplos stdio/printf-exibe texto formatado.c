798dccdeb6f62499a612328f81b8802af06a7e52
mdc-cod <mdcoconta@gmail.com>
2026-06-24 19:32:48 -0300

    atualização de printf-exibe texto formatado.c

--- C/exemplos stdio/printf-exibe texto formatado.c
+++ C/exemplos stdio/printf-exibe texto formatado.c
@@ -4,7 +4,7 @@ int main(){
 
 int val=1;
 
-    printf("exibe \t\ttexto \nformatado \nna tela, e valores como de variaveis . Exemplo: mostrar valor da variável val: %d", val);
+    printf("exibe \t\ttexto \nformatado \nna tela, e valores como de variaveis . Exemplo: \n\tmostrar valor da variável val: %d", val);
 
   return 0;
  
