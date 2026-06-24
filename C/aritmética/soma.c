ffb831c46abd1804e8f0940b28474273156b667b
mdc-cod <mdcoconta@gmail.com>
2026-06-24 09:04:15 -0300

    comentário adicionado

--- C/aritmética/soma.c
+++ C/aritmética/soma.c
@@ -4,8 +4,11 @@ int main(){
 
   int n1=10;
   int n2=5;
-  int soma = n1+n2;
-     printf("resultado: %d", soma);
+
+  int soma = n1 + n2; // soma n1 +n2
+
+  printf("resultado: %d", soma);
+  
   return 0;
 
 }
