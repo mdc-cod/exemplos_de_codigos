55adc4a72ee39201daf0c5a19fb15de8d593c68b
mdc-cod <mdcoconta@gmail.com>
2026-06-24 20:21:55 -0300

    arquivo fputc-grava caractere no arquivo.c criado

---
+++ C/exemplos stdio/fputc-grava caractere no arquivo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+  FILE *letra = fopen("a.txt", "w"); // cria o arquivo para escrita 
+
+  fputc('a', letra); // grava a letra 'a' no arquivo
+
+  fclose(letra);// fecha o arquivo 
+}
