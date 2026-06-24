b1b017e028d07a4d32185b1e111e50a1b89668f2
mdc-cod <mdcoconta@gmail.com>
2026-06-24 13:33:07 -0300

    arquivo strcat-concatena (junta) duas strings.c criado

---
+++ C/exemplos string/strcat-concatena (junta) duas strings.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+  char a[] = "anti";
+  char b[] = "virus";
+
+  printf("%s", strcat(a, b)); // cancatena e exibe as strings a e b concatenadas
+
+  return 0;
+
+}
