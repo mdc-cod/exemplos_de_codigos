8767273c4ba78714d96ee728247259b8d525d15b
mdc-cod <mdcoconta@gmail.com>
2026-06-24 20:00:12 -0300

    arquivo FILE-cria arquivo.c atualizado
    novos comentários adicionados

--- C/exemplos stdio/FILE-cria arquivo.c
+++ C/exemplos stdio/FILE-cria arquivo.c
@@ -1,11 +1,21 @@
+
+  /*
+   * "w" (write) cria arquivo para escrita, se o arquivo já existir o conteúdo 
+   *  do arquivo será substituído pelo novo conteudo
+   *
+   * "a" (append) cria o arquivo , se não existir, para escrita, 
+   *se o arquivo já existir o conteúdo a ser escrito será adicionado no final do arquivo
+   *
+   * "r" faz a leitura do arquivo
+   */
+  
+
 #include <stdio.h>
 
-
 int main(){
-
+ 
   FILE *arquivo = fopen("nome_do_arquivo.txt", "w"); //cria arquivo do tipo txt para "w" (write) escrita
-
-
- fclose(arquivo); // fecha o arquivo 
+  
+  fclose(arquivo); // fecha o arquivo 
 
 }
