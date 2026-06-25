776466aa64d8291e4b2cfe4c056279289ce39175
mdc-cod <mdcoconta@gmail.com>
2026-06-25 10:39:27 -0300

    arquivo fgets-obtem string do buffer de entrada (digitado pelo usuário).c criado

---
+++ C/exemplos stdio/fgets-obtem string do buffer de entrada (digitado pelo usuário).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+
+ // matriz de caracteres com 20 de capacidade 
+  char veiculo[20]; // onde a strig obtida será armazenado 
+
+  printf("tipo de veiculo:\n"); 
+
+  // matriz, capacidade da matriz, buffer de entrada (local dos valores digitados)
+  fgets(veiculo, 20, stdin); // obtém a strig digitada pelo usuario
+
+  printf("tipo do veiculo: %s", veiculo); // exibe a strig obtida
+
+  return 0;
+
+}
