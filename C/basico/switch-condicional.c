471b7f74ef4a2aab76bcc4ee1f4b074aa172a2d5
mdc-cod <mdcoconta@gmail.com>
2026-06-25 22:47:06 -0300

    arquivo switch-condicional.c criado

---
+++ C/basico/switch-condicional.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main(){
+
+  int dia_da_semana = 5;
+
+  switch(dia_da_semana){
+
+   case 1: 
+
+      printf("dom");
+
+   break;
+  
+   case 2:
+
+      printf("segunda-feira");
+
+   break;
+
+   case 3:
+
+      printf("terca-feira");
+
+   break;
+
+   case 4:
+
+     printf("quarta-feira");
+
+   break;
+
+   case 5:
+
+      printf("quinta-feira");
+
+   break;
+
+   case 6:
+
+     printf("sexta-feira");
+
+   break;
+
+   case 7:
+
+    printf("sabado");
+
+   break;
+  
+
+
+  }
+
+  return 0;
+
+}
