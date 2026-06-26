0c0e3f1fba9be0fbf581484fe18adebe7026443c
mdc-cod <mdcoconta@gmail.com>
2026-06-25 22:22:11 -0300

    arwuivo if_else_if-condicional.c criado

---
+++ C/basico/if_else_if-condicional.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+  int v1 = 5;
+  int v2 = 5;
+
+  if(v1>v2){ // se v1 for maior que v2
+
+    printf("v1 maior que v2");
+
+  } else if(v1 == v2){ // se v1 for igual a v2
+
+    printf("v1 igual a v2");
+
+  } else { // v1 menor que v2
+
+    printf("v1 menor que v2");
+
+  }
+
+}
