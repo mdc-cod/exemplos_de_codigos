a5b20a169d8e2a689db239b3a0212b7a4abf6a62
mdc-cod <mdcoconta@gmail.com>
2026-06-25 22:15:13 -0300

    arquivo if_else-condicional.c criado

---
+++ C/basico/if_else-condicional.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+  int v1 = 1;
+  int v2 = 5;
+
+  if(v1 > v2){ // se v1 for maior que v2
+
+    printf("v1 maior que v2");
+  
+  } else { // se v1 for menor ou igual a v2
+
+    printf("v1 menor ou igual a v2");
+
+  }
+
+
+}
