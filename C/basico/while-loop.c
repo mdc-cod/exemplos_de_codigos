949e338a728e1b3fecde56cc91224a80361e53e3
mdc-cod <mdcoconta@gmail.com>
2026-06-25 22:30:19 -0300

    arquivo while-loop.c criado

---
+++ C/basico/while-loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+  int contagem=1;
+
+  // enquanto o contagem menor que 100 o loop se matem
+  while(contagem < 100){ 
+
+     contagem++; // acrescenta +1 
+     printf("%d",contagem); // exibe a contagem 
+
+  }
+
+  return 0;
+
+}
